65e8dcfa8342b7f8b7e8f0bd2cab1c689d758601
Wei-Cheng Wu <spencerwu85@gmail.com>
2017-03-19 22:49:04 +0800

    Add error detections

--- v1/ubike-station/taipei/index.php
+++ v1/ubike-station/taipei/index.php
@@ -31,16 +31,16 @@ if( $_GET['lat'] && $_GET['lng'] )
 		$myObj->code = 0;
 		$myObj->result = [];
 
-        $target = find( $lat, $lng);
+		$target = find( $lat, $lng);
 
-	$good1 = new stdClass;
-	$good1->station = $target[0]->sna;
-	$good1->num_bike = $target[0]->sbi;
-	$good2 = new stdClass;
-	$good2->station = $target[1]->sna;
-	$good2->num_bike = $target[1]->sbi;
-	array_push($myObj->result, $good1);
-	array_push($myObj->result, $good2);
+		$good1 = new stdClass;
+		$good1->station = $target[0]->sna;
+		$good1->num_bike = $target[0]->sbi;
+		$good2 = new stdClass;
+		$good2->station = $target[1]->sna;
+		$good2->num_bike = $target[1]->sbi;
+		array_push($myObj->result, $good1);
+		array_push($myObj->result, $good2);
 	}
 	else
 	{
@@ -52,5 +52,15 @@ if( $_GET['lat'] && $_GET['lng'] )
 	$output = json_encode($myObj, JSON_UNESCAPED_UNICODE);
 	echo $output;
 }
+else 
+{
+	$myObj = new stdClass;
+		$myObj->code = -1;
+		$myObj->result = [];
 
+	header('Content-type: application/json');
+	$output = json_encode($myObj, JSON_UNESCAPED_UNICODE);
+	echo $output;
+
+}
 
